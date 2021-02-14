ebe0ad8e76e5074c58efc6439265558879d4919a
Tuğsan Ünlü <unlutugsan@gmail.com>
2021-02-14 22:12:26 +0300

    feat: add lru cache for API calls

--- utils/axios.js
+++ utils/axios.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
+import { cacheAdapterEnhancer } from 'axios-extensions';
+import LRUCache from 'lru-cache';
 import { APP } from './constants';
+
+const defaultCache = new LRUCache({ maxAge: 1000 * 60 * 60 }); // 1 hour
 
 const API = axios.create({
   baseURL: APP.API_URL,
+  adapter: cacheAdapterEnhancer(axios.defaults.adapter, {
+    defaultCache,
+  }),
 });
 
 export { API };
